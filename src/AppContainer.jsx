952af727db818d75140c6e99c207579e3caefd94
RazvanB <45710607+RazvanBrinzoi@users.noreply.github.com>
2025-08-26 17:06:56 +0300

    Create statistics logic for insta and facebook

--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -2,17 +2,26 @@ import StatisticsWrapper from "./StatisticsWrapper";
 import Textarea from "./Textarea";
 import StatisticsCard from "./StatisticsCard";
 import { useMemo, useState } from "react";
+import { FACEBOOK_CHAR_LIMIT, INSTAGRAM_CHAR_LIMIT } from "./constants";
 
 export default function AppContainer() {
   const [text, setText] = useState("");
 
   // Optimised computations with useMemo
   const textWordCount = useMemo(() => {
-    return text.trim().length > 0 ? text.trim().split(/\s+/).length : 0; //Împarte textul folosind regex-ul \s+ (una sau mai multe spații, taburi, newline). Pentru a evita cuvinte goale
+    return text.trim().length > 0 ? text.trim().split(/\s+/).length : 0; //Split text using regex \s+ (one ore more spaces, tabs, newlines) to avoid empty string values.
   }, [text]);
 
   // Optimised computations with useMemo
   const textCharacterCount = useMemo(() => text.length, [text]);
+  const instagramCharactersLeft = useMemo(
+    () => INSTAGRAM_CHAR_LIMIT - textCharacterCount,
+    [textCharacterCount]
+  );
+  const facebookCharactersLeft = useMemo(
+    () => FACEBOOK_CHAR_LIMIT - textCharacterCount,
+    [textCharacterCount]
+  );
 
   return (
     <main className="container">
@@ -20,8 +29,16 @@ export default function AppContainer() {
       <StatisticsWrapper>
         <StatisticsCard wordCount={textWordCount} label={"Words"} />
         <StatisticsCard wordCount={textCharacterCount} label={"Characters"} />
-        <StatisticsCard wordCount={280} label={"Instagram"} />
-        <StatisticsCard wordCount={2200} label={"Facebook"} />
+        <StatisticsCard
+          wordCount={instagramCharactersLeft}
+          label={"Instagram"}
+          limitReached={instagramCharactersLeft < 0}
+        />
+        <StatisticsCard
+          wordCount={facebookCharactersLeft}
+          label={"Facebook"}
+          limitReached={facebookCharactersLeft < 0}
+        />
       </StatisticsWrapper>
     </main>
   );
--- src/StatisticsCard.jsx
+++ src/StatisticsCard.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-function StatisticsCard({ wordCount, label }) {
+function StatisticsCard({ wordCount, label, limitReached }) {
   return (
     <div className="stat">
-      <p className="stat__number"> {wordCount} </p>
+      <p
+        className={`stat__number ${limitReached ? "stat__number--limit" : ""}`}
+      >
+        {" "}
+        {wordCount}{" "}
+      </p>
       <h2 className="second-heading"> {label}</h2>
     </div>
   );
 }
 
+//Memo becomes useless now because all cards must rerender at text change but will keep it as informative
 export default React.memo(StatisticsCard);
