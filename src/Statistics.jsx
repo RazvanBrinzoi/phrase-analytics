01bada55cea6df0664d93ca8c232f81c14a2388a
RazvanB <45710607+RazvanBrinzoi@users.noreply.github.com>
2025-08-26 13:58:19 +0300

    Structuring and styling the app

---
+++ src/Statistics.jsx
@@ -0,0 +1,22 @@
+export default function Statistics() {
+  return (
+    <section className="stats">
+      <div className="stat">
+        <p className="stat__number"> 0 </p>
+        <h2 className="second-heading"> Words</h2>
+      </div>
+      <div className="stat">
+        <p className="stat__number"> 0 </p>
+        <h2 className="second-heading"> Characters</h2>
+      </div>
+      <div className="stat">
+        <p className="stat__number"> 280 </p>
+        <h2 className="second-heading"> Instagram</h2>
+      </div>
+      <div className="stat">
+        <p className="stat__number"> 2200 </p>
+        <h2 className="second-heading"> Facebook</h2>
+      </div>
+    </section>
+  );
+}
